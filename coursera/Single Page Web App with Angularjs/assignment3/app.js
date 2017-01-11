78bc5f8ae41ab883864dac93d9be484bd8c23ea8
Yaoming Jiang <yao4ming@gmail.com>
2017-01-10 23:24:46 -0500

    assignment3

--- coursera/Single Page Web App with Angularjs/assignment3/app.js
+++ coursera/Single Page Web App with Angularjs/assignment3/app.js
@@ -1,26 +1,8 @@
 angular.module('NarrowItDownApp', [])
 .constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com')
-.controller('NarrowItDownController', ['MenuSearchService', function(MenuSearchService) {
-	var ctrl = this;
-	this.getMatchedMenuItems = function(searchTerm) {
-		var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
-		promise.then(function(response) {
-			console.log(response);
-			ctrl.found = response;
-		})
-		.catch(function(error) {
-			console.log("getMatchedMenuItems callback failed");
-		});
-	}
-
-	ctrl.removeItem = function(index) {
-		ctrl.found.splice(index, 1);
-	}
-}])
 .service('MenuSearchService', ['$http', 'ApiBasePath', function($http, ApiBasePath) {
 
 	this.getMatchedMenuItems = function(searchTerm) {
-		console.log(searchTerm);
 		return $http({
 			url: ApiBasePath + '/menu_items.json'
 		}).then(function (result) {
@@ -36,8 +18,28 @@ angular.module('NarrowItDownApp', [])
 	}
 	return this;
 }])
+.controller('NarrowItDownController', ['MenuSearchService', function(MenuSearchService) {
+	var ctrl = this;
+	ctrl.getMatchedMenuItems = function(searchTerm) {
+		var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+		promise.then(function(response) {
+			ctrl.found = response;
+		})
+		.catch(function(error) {
+			console.log("getMatchedMenuItems callback failed");
+		});
+	}
+
+	ctrl.removeItem = function(index) {
+		ctrl.found.splice(index, 1);
+	}
+}])
 .directive('foundItems', function() {
 	return {
-		templateUrl: './foundItems.html'
+		templateUrl: './foundItems.html',
+		scope: {
+			foundItems: '=found',
+			onRemove: '&'
+		}	
 	}
 });
